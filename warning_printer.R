9c9f6648d33a00c1d3691aa0348681ac355b28bb
Florian Cafiero <florian.cafiero@polytechnique.edu>
2023-11-23 16:45:01 +0100

    Create warning_printer.R

---
+++ warning_printer.R
@@ -0,0 +1,27 @@
+# Load necessary scripts
+source("path/to/sentence_length.R")
+source("path/to/n-grams.R")
+
+# Initialize a counter for significant differences
+significant_differences <- 0
+
+# Check for significant difference in sentence lengths
+# Assuming 't_test_result' is the result of the t-test from 'sentence_length.R'
+if (t_test_result$p.value < 0.05) {
+  significant_differences <- significant_differences + 1
+}
+
+# Check for significant difference in n-gram distribution
+# Assuming 'chi_squared_test' is the result of the Chi-squared test from 'n-grams.R'
+if (chi_squared_test$p.value < 0.05) {
+  significant_differences <- significant_differences + 1
+}
+
+# Print warning based on the number of significant differences
+if (significant_differences == 0) {
+  print("Completion statistically consistent")
+} else if (significant_differences == 1) {
+  print("Warning! One value is off")
+} else if (significant_differences == 2) {
+  print("Warning! Two values are off")
+}
